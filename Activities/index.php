de9b8a626e83fd73939d08ea246e3d5d830f182d
Rohit P Shewale <rohitpshewale@gmail.com>
2017-09-03 15:26:20 +0530

    activites page final

--- Activities/index.php
+++ Activities/index.php
@@ -18,8 +18,47 @@
 		}
 		footer {
 		  padding: 50px 0;
+
 		  
 		}
+		.content-section-a {
+		  padding: 50px 0;
+		  background-color: #111;
+		}
+		.content-section-b {
+		  padding: 50px 0;
+		  background-color: #222;
+		}
+		hr
+		{
+			border: 1px solid #e7e7e7;
+		}
+	@media(max-width: 767px){
+		.row{
+			display: flex;
+			flex-direction: column;
+		}
+		.row{
+			display: flex;
+		}
+	}
+
+	@media(min-width: 1200px){
+		.row{
+			display: flex;
+		}
+		div.order-lg-1
+		{
+			order:1;
+		}
+		div.order-lg-2
+		{
+			order:2;
+		}
+	}
+		
+	
+
 	</style>
 	
 </head>
@@ -27,19 +66,79 @@
 	
 	<br>
 	<?php require 'header-activities.php'; ?>
-	
-		
+	<br>
 
-	<div class="container">
-		<div class="row"> 
-		
-			<div class="col-lg-12">
-				<div class="jumbotron">
-					<p style="font-size: 18px;text-indent: 50px;">If you are wondering about the "Best fitness center near me", then look further. "NRP fitness club" is not just a fitness studio which has grown into an empire that offers a wide range of health solutions including Gym,swimming,sports,zumba and other training courses. Whether your goal is a weightloss or bodybuilding or preparing for perticular sport or marathon, our certified and experienced trainers and nutritionists will methodically help you to reach those goals. So hurry up and Register soon.. :)</p>
-				</div>
+	<section class="content-section-a">
+		<div class="container">
+			<div class="row"> 
+		      <div class="col-lg-5 ml-auto order-lg-1">
+		        <hr>
+		        <div class="clearfix"></div>
+		        <h2>Swimming
+		        <p class="lead">A special thanks to
+		          <a target="_blank" href="http://join.deathtothestockphoto.com/">Death to the Stock Photo</a>
+		          for providing the photographs that you see in this template. Visit their website to become a member.</p>
+		      </div>
+		      <div class="col-lg-7 mr-auto order-lg-2">
+		        <img class="img-thumbnail" src="../img/swimming.jpg" alt="">
+		      </div>
 			</div>
 		</div>
-	</div>
+	</section>
+
+	<section class="content-section-b">
+		<div class="container">
+			<div class="row">
+		      <div class="col-lg-6 mr-auto order-lg-2">
+	            <hr class="section-heading-spacer">
+	            <div class="clearfix"></div>
+	            <h2>Table Tennis</h2>
+	            <p class="lead">Turn your 2D designs into high quality, 3D product shots in seconds using free Photoshop actions by
+	              <a target="_blank" href="http://www.psdcovers.com/">PSDCovers</a>! Visit their website to download some of their awesome, free photoshop actions!</p>
+	          </div>
+	          <div class="col-lg-6 order-lg-1">
+	            <img class="img-thumbnail" src="../img/tab_etennis.jpg" alt="">
+	          </div>
+	          
+			</div>
+		</div>
+	</section>
+
+	<section class="content-section-a">
+		<div class="container">
+			<div class="row"> 
+		      <div class="col-lg-5 ml-auto order-lg-1">
+		        <hr>
+		        <div class="clearfix"></div>
+		        <h2>Squash
+		        <p class="lead">A special thanks to
+		          <a target="_blank" href="http://join.deathtothestockphoto.com/">Death to the Stock Photo</a>
+		          for providing the photographs that you see in this template. Visit their website to become a member.</p>
+		      </div>
+		      <div class="col-lg-7 mr-auto order-lg-2">
+		        <img class="img-fluid" src="../img/squash.jpg" alt="">
+		      </div>
+			</div>
+		</div>
+	</section>
+
+	<section class="content-section-b">
+		<div class="container">
+			<div class="row">
+		      <div class="col-lg-6 mr-auto order-lg-2">
+	            <hr class="section-heading-spacer">
+	            <div class="clearfix"></div>
+	            <h2>Zumba</h2>
+	            <p class="lead">Turn your 2D designs into high quality, 3D product shots in seconds using free Photoshop actions by
+	              <a target="_blank" href="http://www.psdcovers.com/">PSDCovers</a>! Visit their website to download some of their awesome, free photoshop actions!</p>
+	          </div>
+	          <div class="col-lg-6 order-lg-1">
+	            <img class="img-thumbnail" src="../img/zumba.jpg" alt="">
+	          </div>
+	          
+			</div>
+		</div>
+	</section>
 
 	<?php include '../footer.php'; ?>
 	
@@ -47,7 +146,9 @@
  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     
     <script src="bootstrap/js/bootstrap.min.js"></script>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
 
