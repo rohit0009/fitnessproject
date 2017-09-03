fa0862d310d80f8305dd06582bbc6d5b71facc13
Rohit P Shewale <rohitpshewale@gmail.com>
2017-09-03 15:45:17 +0530

    Update index.php

--- Activities/index.php
+++ Activities/index.php
@@ -142,12 +142,7 @@
 
 	<?php include '../footer.php'; ?>
 	
-	
- 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    
-    <script src="bootstrap/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    
     <script src="../bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
