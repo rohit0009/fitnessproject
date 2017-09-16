74215e0059f821fd4ae4fab8b313e5baa260a3d9
Rohit P Shewale <rohitpshewale@gmail.com>
2017-09-16 22:52:47 +0530

    100%done update member

--- site-admin/home.php
+++ site-admin/home.php
@@ -61,8 +61,9 @@
 				$("#updateMtable tr:eq(1) td:eq(0)").text(data[0]);
 				$("#updateMtable tr:eq(1) td:eq(1)").text(data[3]);
 				$("#updateMtable tr:eq(1) td:eq(2)").text(data[4]);
-				$("#updateMtable tr:eq(1) td:eq(3)").text("1232");
-				$("#updateMtable tr:eq(1) td:eq(4)").text(data[2]);
+				$("#updateMtable tr:eq(1) td:eq(3)").text(data[5]);
+				$("#updateMtable tr:eq(1) td:eq(4)").text(data[6])
+				$("#updateMtable tr:eq(1) td:eq(5)").text(data[2]);
 			}
 		});
 
@@ -149,7 +150,94 @@
 							});
 						</script>";
 				}
+			}
+			if(isset($_REQUEST['updateMember']))
+			{
+				if($_REQUEST['attribute']=="" || $_REQUEST['selectUpdateM']=="" || $_REQUEST['newdetails']=="")
+				{
+					echo '<br><div class="alert alert-dismissible alert-danger">
+						  <button type="button" class="close" data-dismiss="alert">&times;</button>
+						  <strong>Enter all details.</div>';
+					echo "<script>
+							$(document).ready(function(){ 
+								$('#Courses').removeClass('active in'); 
+								$('#updateM').addClass('active in');
+								$('#innertab > li:nth-child(1)').removeClass('active');
+								$('#innertab > li:nth-child(3)').addClass('active');
+							});
+						</script>";
+				}
+				else
+				{
+					$arr = explode("^&^", $_REQUEST['selectUpdateM']);
+					$flag = 1;
+					if($flag)
+					{
+						if($_REQUEST['attribute'] == "contact_no")
+						{
+							if(!is_numeric($_REQUEST['newdetails']))
+							{
+								echo '<br><div class="alert alert-dismissible alert-warning">
+						 		  <button type="button" class="close" data-dismiss="alert">&times;</button>
+						 	  	<strong>Enter Numbers only.</strong></div>';
+						 	  	$flag = 0;
+						 	}
+						 	  else
+						 	  {
+						 	  		if(strlen($_REQUEST['newdetails']) != 10)
+						 	  		{
+						 	  			echo '<br><div class="alert alert-dismissible alert-warning">
+									 		  <button type="button" class="close" data-dismiss="alert">&times;</button>
+									 	  	<strong>Enter 10 numbers only.</strong></div>';
+									 	$flag = 0;  	
+						 	  		}
+						 	  }
+						}
+						if($_REQUEST['attribute'] == "pincode")
+						{
+							if(!is_numeric($_REQUEST['newdetails']))
+							{
+								echo '<br><div class="alert alert-dismissible alert-warning">
+						 		  <button type="button" class="close" data-dismiss="alert">&times;</button>
+						 	  	<strong>Enter Numbers only.</strong></div>';
+						 	  	$flag = 0;
+						 	}
+						 	  else
+						 	  {
+						 	  		if(strlen($_REQUEST['newdetails']) != 6)
+						 	  		{
+						 	  			echo '<br><div class="alert alert-dismissible alert-warning">
+									 		  <button type="button" class="close" data-dismiss="alert">&times;</button>
+									 	  	<strong>Enter 6 numbers only.</strong></div>';
+									 	$flag = 0;
+						 	  		}
+						 	  }
+						}
+					}
 
+					if($flag)
+					{
+						$result = $dtb->processQuery('update member set '.$_REQUEST['attribute'].'='.$_REQUEST['newdetails'].' where cust_id='.$arr[0]);
+						if($result === TRUE)
+							echo '<br><div class="alert alert-dismissible alert-success">
+								  <button type="button" class="close" data-dismiss="alert">&times;</button>
+							  	<strong>Updated Member Details.</div>';
+						else
+							echo '<br><div class="alert alert-dismissible alert-warnng">
+								  <button type="button" class="close" data-dismiss="alert">&times;</button>
+							  	<strong>Failed.</div>';
+				    }
+					
+					//print_r($arr);
+					echo "<script>
+							$(document).ready(function(){ 
+								$('#Courses').removeClass('active in'); 
+								$('#updateM').addClass('active in');
+								$('#innertab > li:nth-child(1)').removeClass('active');
+								$('#innertab > li:nth-child(3)').addClass('active');
+							});
+						</script>";	
+				}
 			}
 			$dtb->close();
 		} 
@@ -166,21 +254,21 @@
 					  <div class="panel-heading">
 					    <h3 class="panel-title">Update Member Details</h3>
 					  </div>
-				
 					<div class="panel-body">
 						<form class="form-horizontal" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 							<fieldset>
 								<div class="form-group">
+
 							    	<label for="select" class="col-lg-2 control-label">Select Name</label>
 						      		<div class="col-lg-2">
 								        <select class="form-control" id="selectUpdateM" name="selectUpdateM">
 								        	<?php
 												$dtb = new DTB();
-												$result = $dtb->processQuery("select cust_id,email,username,f_name,l_name from member;");
+												$result = $dtb->processQuery("select cust_id,email,username,f_name,l_name,pincode,contact_no from member;");
 												echo "<option class='disabled' value=''></option>";	
 												while ($row = $result->fetch_assoc())
 												{
-													echo '<option value='.$row["cust_id"]."^&^".$row["email"]."^&^".$row['username']."^&^".$row['f_name']."^&^".$row['l_name']."^&^".$row['contact_no'].'>'.$row["f_name"]." ".$row["l_name"].'</option>';
+													echo '<option value='.$row["cust_id"]."^&^".$row["email"]."^&^".$row['username']."^&^".$row['f_name']."^&^".$row['l_name']."^&^".$row['contact_no']."^&^".$row['pincode'].'>'.$row["f_name"]." ".$row["l_name"].'</option>';
 												}
 												$dtb->close();
 											?>
@@ -203,7 +291,7 @@
 							<div class="form-group">
 								<label class="col-lg-3 control-label">Select attribute to modify</label>
 								<div class="col-lg-3">
-						       		<select class="form-control" id="attribute">
+						       		<select class="form-control" name="attribute" id="attribute">
 						       		<option value=""></option>
 						       		<option value="contact_no">Contact Number</option>
 						       		<option value="pincode">Pincode</option>
@@ -217,31 +305,38 @@
 						        	<input type="text" class="form-control" name="newdetails" id="newdetails"></input>
 						        </div>
 							</div>
+							<div class="form-group">
+						      <div class="col-lg-5 col-lg-offset-3">
+						        <button type="submit" class="btn btn-warning" id="updateMember" name="updateMember">Update Member Details</button>
+						      </div>
+						    </div>
 							<hr style="border-top: 1px grey solid;width: 95%; margin-left: 30px;">
 							<div class="form-group">
-							<div class="container-fluid">
-									<table class="table table-striped" id="updateMtable">
-										<thead>
-										    <tr>
-												<th>Customer ID</th>
-												<th>First name</th>
-												<th>Last name</th>
-												<th>Contact Number</th>
-												<th>Username</th>
-											</tr>
-										  </thead>
-										  <tbody>
-										  	<tr>
-										  		<td></td>
-										  		<td></td>
-										  		<td></td>
-										  		<td></td>
-										  		<td></td>
-										  	</tr>
-										  </tbody>
-									</table>
+								<div class="container-fluid">
+										<table class="table table-striped" id="updateMtable">
+											<thead>
+											    <tr>
+													<th>Customer ID</th>
+													<th>First name</th>
+													<th>Last name</th>
+													<th>Contact Number</th>
+													<th>Pincode</th>
+													<th>Username</th>
+												</tr>
+											  </thead>
+											  <tbody>
+											  	<tr>
+											  		<td></td>
+											  		<td></td>
+											  		<td></td>
+											  		<td></td>
+											  		<td></td>
+											  		<td></td>
+											  	</tr>
+											  </tbody>
+										</table>
+									</div>
 								</div>
-							</div>
 						</form>
 					</div>
 				</div>
