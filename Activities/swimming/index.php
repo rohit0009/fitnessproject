dabcf613e9a0ec47f945099e8c25aae1254fd9c8
Rohit P Shewale <rohitpshewale@gmail.com>
2017-09-20 17:40:51 +0530

    design fix

--- Activities/swimming/index.php
+++ Activities/swimming/index.php
@@ -29,32 +29,41 @@
 		$batch = $dtb->processQuery("select batch_id,batch_name,batch_time from batch where course_id = ".$course_id.";");
 		
 		echo'<div class="container">
-			<table class="table table-striped table-hover">
-				<thead>
-					<tr>
-						<th>Batch</th>
-						<th>Trainer</th>
-						<th>Seats</th>
-					</tr>
-					</thead>
-					<tbody>';
-						
-						while($batchrow = $batch->fetch_assoc())
-						{
-							echo '<tr>';
-							echo '<td>'.$batchrow['batch_name'].' &nbsp&nbsp(&nbsp'.$batchrow['batch_time'].'&nbsp)</td>';
+				<div class="row">
+					<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+						<div class="panel panel-info">
+							<div class="panel-heading">
+								Batch-Trainer Details
+							</div>
+							<table class="table table-striped table-hover">
+								<thead>
+									<tr>
+										<th>Batch</th>
+										<th>Trainer</th>
+										<th>Seats</th>
+									</tr>
+									</thead>
+									<tbody>';
+										
+										while($batchrow = $batch->fetch_assoc())
+										{
+											echo '<tr>';
+											echo '<td>'.$batchrow['batch_name'].' &nbsp&nbsp(&nbsp'.$batchrow['batch_time'].'&nbsp)</td>';
 
-							$trainer = $dtb->processQuery("select trainer.trainer_id,trainer_name,no_of_seats from trainer,seat where seat.course_id = ".$course_id." and seat.course_id = trainer.course_id and trainer.trainer_id = seat.trainer_id and seat.batch_id = ".$batchrow['batch_id'].";");
+											$trainer = $dtb->processQuery("select trainer.trainer_id,trainer_name,no_of_seats from trainer,seat where seat.course_id = ".$course_id." and seat.course_id = trainer.course_id and trainer.trainer_id = seat.trainer_id and seat.batch_id = ".$batchrow['batch_id'].";");
 
-							$trainerrow = $trainer->fetch_assoc();
-							
-							echo '<td>'.$trainerrow['trainer_name'].'</td>';
-							echo '<td>'.$trainerrow['no_of_seats'].'</td>';	
-							
-							echo '</tr>';
-						}
-					echo '</tbody>
-				</table>
+											$trainerrow = $trainer->fetch_assoc();
+											
+											echo '<td>'.$trainerrow['trainer_name'].'</td>';
+											echo '<td>'.$trainerrow['no_of_seats'].'</td>';	
+											
+											echo '</tr>';
+										}
+									echo '</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
 			</div>';
 	?>
 
