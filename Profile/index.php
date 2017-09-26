5f06ec2114a73f0a094300fd8c6f2e7f89f9ea8a
Rohit P Shewale <rohitpshewale@gmail.com>
2017-09-26 23:52:57 +0530

    update

--- Profile/index.php
+++ Profile/index.php
@@ -32,121 +32,123 @@
 		{
 			echo "<div class='container-fluid'><br>";
 						$num_of_enrollments = $result->num_rows;
-						$rows_req = ceil($num_of_enrollments/2);
+						//$rows_req = ceil($num_of_enrollments/2);
 						$panel_count = 0;
+						$i_idCounter = 0;
 						while($row = $result->fetch_assoc())
 						{
-								if($panel_count == 0)
-								{
-									echo "<div class='row'>";
-									echo '<div class="col-lg-5 col-lg-offset-1">
-													<div class="panel panel-success">
-													  <div class="panel-heading text-center">';
-									$course = $dtb->processQuery("select course_name from course where course_id = ".$row['course_id']);
-									$course_name = $dtb->getParam($course,"course_name");
-									echo "<h4>".$course_name."</h4>";				  
-									echo  '</div>
-												  <div class="panel-body">
-													  <form class="form-horizontal" method="POST">
-														  <div class="form-group">
-															  <label class="col-lg-3 control-label">Enrollment Date</label>
-															  <div class="col-lg-5">
-															  	<div class="form-control" style="background-color: inherit; color: #AAA;">'.
-												    				$row['enrollment_date'].'</div>
-													  		</div>
-											  			</div>
-											  			<div class="form-group">
-															  <label class="col-lg-3 control-label">Expiry Date</label>
-															  <div class="col-lg-5">
-															  	<div class="form-control" style="background-color: inherit; color: #AAA;">'.
-												    				$row['expiry_date'].'</div>
-													  		</div>
-											  			</div>
-											  			<div class="form-group">
-															  <label class="col-lg-3 control-label">Batch Name</label>
-															  <div class="col-lg-5">
-															  	<div class="form-control" style="background-color: inherit; color: #AAA;">';
-												    				$batch = $dtb->processQuery("select batch_name from batch where batch_id=".$row['batch_id']);
-												    				$batch_name = $dtb->getParam($batch,"batch_name");
-												    				echo $batch_name;
-												    				echo '</div>
-													  		</div>
-											  			</div>
-											  			<div class="form-group">
-															  <label class="col-lg-3 control-label">Batch Time</label>
-															  <div class="col-lg-5">
-															  	<div class="form-control" style="background-color: inherit; color: #AAA;">';
-															  		$batch = $dtb->processQuery("select batch_time from batch where batch_id=".$row['batch_id']);
-												    				$batch_time = $dtb->getParam($batch,"batch_time");
-												    				echo $batch_time;
-											    				echo'</div>
-													  		</div>
-											  			</div>
+							if($panel_count == 0)
+							{
+								echo "<div class='row'>";
+								echo '<div class="col-lg-5 col-lg-offset-1 col-md-5 col-md-offset-1 col-sm-5 col-sm-offset-1">
+												<div class="panel panel-default">
+												  <div class="panel-heading text-center">';
+								$course = $dtb->processQuery("select course_name from course where course_id = ".$row['course_id']);
+								$course_name = $dtb->getParam($course,"course_name");
+								echo "<h5>".$course_name."</h5>";
+								echo  '</div>
+											  <div class="panel-body">
+												  <form class="form-horizontal" method="POST">
+													  <div class="form-group">
+														  <label class="col-lg-3 control-label">Enrollment Date</label>
+														  <div class="col-lg-5">
+														  	<div class="form-control" style="background-color: inherit; color: #AAA;">'.
+											    				$row['enrollment_date'].'</div>
+												  		</div>
+										  			</div>
+										  			<div class="form-group">
+														  <label class="col-lg-3 control-label">Expiry Date</label>
+														  <div class="col-lg-5">
+														  	<div class="form-control" style="background-color: inherit; color: #AAA;">'.
+											    				$row['expiry_date'].'</div>
+												  		</div>
+										  			</div>
+										  			<div class="form-group">
+														  <label class="col-lg-3 control-label">Batch Name</label>
+														  <div class="col-lg-5">
+														  	<div class="form-control" style="background-color: inherit; color: #AAA;">';
+											    				$batch = $dtb->processQuery("select batch_name from batch where batch_id=".$row['batch_id']);
+											    				$batch_name = $dtb->getParam($batch,"batch_name");
+											    				echo $batch_name;
+											    				echo '</div>
+												  		</div>
+										  			</div>
+										  			<div class="form-group">
+														  <label class="col-lg-3 control-label">Batch Time</label>
+														  <div class="col-lg-5">
+														  	<div class="form-control" style="background-color: inherit; color: #AAA;">';
+														  		$batch = $dtb->processQuery("select batch_time from batch where batch_id=".$row['batch_id']);
+											    				$batch_time = $dtb->getParam($batch,"batch_time");
+											    				echo $batch_time;
+										    				echo'</div>
+												  		</div>
+										  			</div>
 
-											   		</form>
-											  		</div>
-													</div>
-											</div>';
-								}
-								else if($panel_count == 1)
-								{
-									echo '<div class="col-lg-5">
-													<div class="panel panel-success">
-													  <div class="panel-heading text-center">';
-									$course = $dtb->processQuery("select course_name from course where course_id = ".$row['course_id']);
-									$course_name = $dtb->getParam($course,"course_name");
-									echo "<h4>".$course_name."</h4>";
-									echo  '</div>
-												  <div class="panel-body">
-													  <form class="form-horizontal" method="POST">
-														  <div class="form-group">
-															  <label class="col-lg-3 control-label">Enrollment Date</label>
-															  <div class="col-lg-5">
-															  	<div class="form-control" style="background-color: inherit; color: #AAA;">'.
-												    				$row['enrollment_date'].'</div>
-													  		</div>
-											  			</div>
-											  			<div class="form-group">
-															  <label class="col-lg-3 control-label">Expiry Date</label>
-															  <div class="col-lg-5">
-															  	<div class="form-control" style="background-color: inherit; color: #AAA;">'.
-												    				$row['expiry_date'].'</div>
-													  		</div>
-											  			</div>
-											  			<div class="form-group">
-															  <label class="col-lg-3 control-label">Batch Name</label>
-															  <div class="col-lg-5">
-															  	<div class="form-control" style="background-color: inherit; color: #AAA;">';
-												    				$batch = $dtb->processQuery("select batch_name from batch where batch_id=".$row['batch_id']);
-												    				$batch_name = $dtb->getParam($batch,"batch_name");
-												    				echo $batch_name;
-												    				echo '</div>
-													  		</div>
-											  			</div>
-											  			<div class="form-group">
-															  <label class="col-lg-3 control-label">Batch Time</label>
-															  <div class="col-lg-5">
-															  	<div class="form-control" style="background-color: inherit; color: #AAA;">';
-															  		$batch = $dtb->processQuery("select batch_time from batch where batch_id=".$row['batch_id']);
-												    				$batch_time = $dtb->getParam($batch,"batch_time");
-												    				echo $batch_time;
-											    				echo'</div>
-													  		</div>
-											  			</div>
+										   		</form>
+										  		</div>
+												</div>
+										</div>';
+							}
+							else if($panel_count == 1)
+							{
+								echo '<div class="col-lg-5 col-md-5 col-sm-5">
+												<div class="panel panel-default">
+												  <div class="panel-heading text-center">';
+								$course = $dtb->processQuery("select course_name from course where course_id = ".$row['course_id']);
+								$course_name = $dtb->getParam($course,"course_name");
+								echo "<h5>".$course_name."</h5>";
+								echo  '</div>
+											  <div class="panel-body">
+												  <form class="form-horizontal" method="POST">
+													  <div class="form-group">
+														  <label class="col-lg-3 control-label">Enrollment Date</label>
+														  <div class="col-lg-5">
+														  	<div class="form-control" style="background-color: inherit; color: #AAA;">'.
+											    				$row['enrollment_date'].'</div>
+												  		</div>
+										  			</div>
+										  			<div class="form-group">
+														  <label class="col-lg-3 control-label">Expiry Date</label>
+														  <div class="col-lg-5">
+														  	<div class="form-control" style="background-color: inherit; color: #AAA;">'.
+											    				$row['expiry_date'].'</div>
+												  		</div>
+										  			</div>
+										  			<div class="form-group">
+														  <label class="col-lg-3 control-label">Batch Name</label>
+														  <div class="col-lg-5">
+														  	<div class="form-control" style="background-color: inherit; color: #AAA;">';
+											    				$batch = $dtb->processQuery("select batch_name from batch where batch_id=".$row['batch_id']);
+											    				$batch_name = $dtb->getParam($batch,"batch_name");
+											    				echo $batch_name;
+											    				echo '</div>
+												  		</div>
+										  			</div>
+										  			<div class="form-group">
+														  <label class="col-lg-3 control-label">Batch Time</label>
+														  <div class="col-lg-5">
+														  	<div class="form-control" style="background-color: inherit; color: #AAA;">';
+														  		$batch = $dtb->processQuery("select batch_time from batch where batch_id=".$row['batch_id']);
+											    				$batch_time = $dtb->getParam($batch,"batch_time");
+											    				echo $batch_time;
+										    				echo'</div>
+												  		</div>
+										  			</div>
 
-											   		</form>
-											  		</div>
-													</div>
-											</div>';	
-								}
-								
-								$panel_count++;
-								if($panel_count == 2)
-								{
-									$panel_count = 0;
-									echo "</div>";
-								}
+										   		</form>
+										  		</div>
+												</div>
+										</div>';	
+							}
+							$panel_count++;
+							if($panel_count == 2)
+							{
+								$panel_count = 0;
+								echo "</div>";
+							}
 						}
+					if($num_of_enrollments%2 != 0)
+						echo "</div>";
 
 					echo "</div>";
 		}
